af5fa5e91e66777068c6e9952b3fdf511330907f
JaielB <150702112+JaielB@users.noreply.github.com>
2024-02-16 10:59:06 -0500

    C:\Users\STU179078\AppData\Roaming\JetBrains\PyCharmCE2023.3\scratches\scratch.py

---
+++ scratch.py
@@ -0,0 +1,16 @@
+
+grades =[100, 85, 94, 50, 67, 81, 49, 55]
+
+type(grades)
+
+min(grades)
+
+max(grades)
+
+print(min(grades))
+
+print(max(grades))
+
+print(sum(grades))
+
+grades.sort(reverse=True)
